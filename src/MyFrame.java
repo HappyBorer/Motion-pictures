c550d2ba570ae8fd7e92cc7468b8764b7a650070
HappyBorer <BorerKicker@yandex.ru>
2024-06-19 08:02:40 +0300

    Created frame

---
+++ src/MyFrame.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+
+    public MyFrame(){
+        super("Motion pictures");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultLookAndFeelDecorated(true);
+        setBounds(500, 200, 600, 600);
+        setVisible(true);
+    }
+}
